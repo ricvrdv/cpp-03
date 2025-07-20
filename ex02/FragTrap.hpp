85c3649fa4f1d5ff9e5e96864db074e8daeaae73
Ricardo Garcia <costagarcia.r95@gmail.com>
2025-07-20 23:15:56 +0100

    feat(ex02): add FragTrap class and member functions definitions

--- ex02/FragTrap.hpp
+++ ex02/FragTrap.hpp
@@ -1,4 +1,20 @@
 #ifndef FRAGTRAP_HPP
 # define FRAGTRAP_HPP
 
+# include "ClapTrap.hpp"
+
+class	FragTrap : public ClapTrap
+{
+	public:
+
+        FragTrap( void );
+        FragTrap( std::string name );
+        FragTrap( FragTrap const &other );
+        FragTrap&   operator=( FragTrap const &other );
+        ~FragTrap( void );
+
+        void    attack( const std::string &target );
+        void    highFivesGuys( void );
+};
+
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,6 +1,40 @@
 #include "FragTrap.hpp"
+#include "ScavTrap.hpp"
 
 int	main()
 {
+	{
+		std::cout << std::endl << "[ CLAPTRAP ]" << std::endl;
+		ClapTrap	robotA;
+        ClapTrap	robotB("CLAP-01");
+
+		robotA.attack("N3M3S1S-01");
+		robotB.attack("N3M3S1S-02");
+		robotB.takeDamage(10);
+		robotB.beRepaired(5);
+	}
+	{
+		std::cout << std::endl << "[ SCAVTRAP ]" << std::endl;
+		ScavTrap	robotC;
+		ScavTrap	robotD("SCAV-01");
+
+		robotC = robotD;
+		robotD.attack("D3V1L-00");
+		robotC.attack("D3V1L-01");
+		robotD.guardGate();
+		robotD.takeDamage(100);
+		robotD.guardGate();
+		robotC.guardGate();
+	}
+	{
+		std::cout << std::endl << "[ FRAGTRAP ]" << std::endl;
+		FragTrap	robotE;
+		FragTrap	robotF("FRAG-01");
+
+		robotF.attack("BRAT-00");
+		robotF.takeDamage(50);
+		robotF.beRepaired(20);
+		robotF.highFivesGuys();
+	}
 	return (0);
 }
